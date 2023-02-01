b2a75d6ae927bf9da8781a8f9204b96537a6d8e3
abhidobariya <120016008+abhidobariya@users.noreply.github.com>
2023-02-01 15:23:02 +0530

    matrix
    new code

---
+++ Practice/matrix_ex1.py
@@ -0,0 +1,8 @@
+from numpy import *
+
+arr1=array([
+        [1,2,3],
+        [4,5,6]
+])
+
+print(arr1.shape)
---
+++ Practice_LJ/ex_240.py
@@ -0,0 +1,29 @@
+# #240 = len find using function:
+# string= (input("Enter String:"))
+# def lenth(string):
+#     count=0
+#     for i in string:
+#         count += 1
+#     print(count)
+# lenth(string)
+
+
+
+# 241 uppercase and lower case count
+#
+# str= ("AbhiPatel0")
+# lower = 0
+# upper = 0
+# for i in str:
+#     if(i.islower()):
+#         lower += 1
+#     else:
+#         upper += 1
+# print("lowercase is:",lower)
+# print("uppercase is:",upper)
+
+
+# 242 negative index in tuple
+
+tup = (1,2,3,4,5,6,7,8,9)
+print(tup.index(4))

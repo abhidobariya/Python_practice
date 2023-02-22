7d8fc8f40ef0600b03f67ed213404ecf38c3db15
abhidobariya <120016008+abhidobariya@users.noreply.github.com>
2023-02-22 17:01:47 +0530

    192
    pattern Add

--- Practice/pattern1.py
+++ Practice/pattern1.py
@@ -62,3 +62,62 @@ for i in range(1,a):
     for j in range(i,0,-1):
         print(i,end=" ")
     print("\r")
+
+
+
+# 1
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+
+a = 6
+
+for i in range(1,a):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print("\r")
+
+
+# 0 1 2 3 4 5
+# 0 1 2 3 4
+# 0 1 2 3
+# 0 1 2
+# 0 1
+
+a = 6
+
+for i in range(a,0,-1):
+    for j in range(0,i+1):
+        print(j,end=" ")
+    print("\r")
+
+
+
+# 5
+# 5 4
+# 5 4 3
+# 5 4 3 2
+# 5 4 3 2 1
+
+a = 5
+
+for i in range(a,0,-1):
+    for j in range(5,i-1,-1):
+        print(j,end=" ")
+    print("\r")
+
+
+
+# 5
+# 4 5
+# 3 4 5
+# 2 3 4 5
+# 1 2 3 4 5
+
+
+a= 6
+for i in range(a,0,-1):
+    for j in range(i,a):
+        print(j,end=" ")
+    print("\r")
